09a4dcecaec184e7d922fdb76fc50b48722412c4
xxKeyaxx <wijayakevin2003@gmail.com>
2026-02-12 18:03:31 +0700

    Hero Section Done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,6 @@ import SocialIcons from "./ui/socialicons";
 import formalPhoto from "../assets/Formal Photo.jpg";
 import CV from "../assets/CV Kevin Wijaya.pdf";
 
-
 const sections = [
   { id: "hero", label: "Home", icon: <FaHome /> },
   { id: "about", label: "About", icon: <FaUser /> },
@@ -51,23 +50,25 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="fixed top-0 left-0 h-screen w-2xs bg-black text-gray-300 flex flex-col items-center py-8">
+    <header className="fixed top-0 left-0 h-screen w-2xs bg-stone-900 text-gray-300 flex flex-col items-center py-8">
       {/* Profile */}
       <div className="flex flex-col items-center">
-        <div className="
+        <div
+          className="
             w-36 h-36
             rounded-full
             p-1
-            bg-[var(--color-accent-gold-dark)]
-            shadow-[0_0_12px_var(--color-accent-gold)]
-            ">
-            <div className="w-full h-full rounded-full overflow-hidden bg-black">
-                <img
-                src={formalPhoto}
-                alt="Profile"
-                className="w-full h-full object-cover"
-                />
-            </div>
+            bg-accent-gold-dark
+            shadow-[0_0_12px_var(--color-accent-gold-dark)]
+            "
+        >
+          <div className="w-full h-full rounded-full overflow-hidden bg-black">
+            <img
+              src={formalPhoto}
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
+          </div>
         </div>
 
         <h1 className="mt-4 text-xl font-semibold text-center">Kevin Wijaya</h1>
@@ -101,7 +102,7 @@ const Header = () => {
                 href={`#${section.id}`}
                 className={`flex items-center gap-3 px-6 py-3 transition ${
                   active === section.id
-                    ? "bg-[var(--color-accent-gold-dark)] text-white"
+                    ? "bg-accent-gold-dark text-white"
                     : "text-gray-400 hover:text-white"
                 }`}
               >
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import React from "react";
 import "../App.css";
+import { Typewriter } from "react-simple-typewriter";
+import CV from "../assets/CV Kevin Wijaya.pdf";
+import { FaFileAlt } from "react-icons/fa";
+import { ChevronDown } from "lucide-react";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  //   const [visible, setVisible] = useState(false);
 
-export default Hero
+  //   useEffect(() => {
+  //     setVisible(true);
+  //   }, []);
+  return (
+    <section className="hero min-h-screen bg-stone-950 text-white">
+      <div className="hero-content text-center">
+        <div className="max-w-3xl space-y-6">
+          {/* Name */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Hi, I'm <span className="text-accent-gold">Kevin Wijaya</span>
+          </h1>
+
+          {/* Typewriter */}
+          <h2 className="text-xl md:text-2xl">
+            <span className="text-[#D4AF37]">
+              <Typewriter
+                words={[
+                  "Software Engineer",
+                  "Backend Developer",
+                  "Data Scientist",
+                ]}
+                loop={0}
+                cursor
+                cursorStyle="|"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1200}
+              />
+            </span>
+          </h2>
+
+          {/* Description */}
+          <p className="text-gray-300">
+            I like to build things that makes a difference. I'm passionate about
+            creating software that solves real-world problems.
+          </p>
+
+          {/* Buttons */}
+          <div className="flex justify-center gap-4 pt-4">
+            <a
+              href={CV}
+              className="btn bg-accent-gold text-black border-none
+                         hover:shadow-[0_0_25px_rgba(212,175,55,0.6)]"
+            >
+              <FaFileAlt /> Download My CV
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/* === Learn More Scroll Indicator === */}
+      <a
+        href="#about"
+        className="absolute bottom-10 flex flex-col items-center text-gray-300 hover:text-accent-gold transition duration-300"
+      >
+        <span className="text-sm tracking-wide mb-2">Learn More About Me</span>
+
+        <ChevronDown size={28} className="animate-bounce" />
+      </a>
+    </section>
+  );
+};
+
+export default Hero;
